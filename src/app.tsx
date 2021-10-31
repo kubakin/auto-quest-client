119972a82e343a2cfd8a8b419e2ce486aa554ee9
d.pogudin <d.pogudin@sysdyn.ru>
2021-10-31 23:15:55 +0300

    modal returned

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LoginModal from './components/loginModal';
 import RegisterModal from './components/registerModal';
 import AuthContext, { IAuthContext } from './context';
-import { addMsg } from './redux/game/gameActions';
+import { addMsg, hideModal } from './redux/game/gameActions';
 import { meAsync } from './redux/user/userAsync';
 import { ModalTypeEnum } from './types/enums';
 import { useTypedSelector } from './__shared/hooks';
@@ -13,13 +13,14 @@ import Chat from './components/chat';
 import WithLoader from './components/withLoader';
 import MainDataProvider from './MainDataProvider';
 import AuthPage from './pages/AuthPage';
+import AlertModal from './components/alertModal';
 
 const App: FC = () => {
     const dispatch = useDispatch();
     const value: IAuthContext = useContext(AuthContext);
     const {
         user: {user, token, userLoaded},
-        game: { gameData },
+        game: { gameData, modal, textModal },
     } = useTypedSelector((state) => state);
 
     useEffect(() => {
@@ -39,6 +40,11 @@ const App: FC = () => {
             <LoginModal
                 handleClose={() => value.setModalType(ModalTypeEnum.none)}
                 show={value.modalType === ModalTypeEnum.signIn && !user}
+            />
+            <AlertModal
+                clickHandler={() => dispatch(hideModal())}
+                visible={modal}
+                text={textModal}
             />
             <Chat
                 handleClose={() => value.setModalType(ModalTypeEnum.none)}
